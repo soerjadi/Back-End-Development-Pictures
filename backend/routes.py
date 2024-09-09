f4526277439a2f78a84a7e3c2ea3aae5543e89dd
soerjadi <soerja.610@gmail.com>
2024-09-09 12:23:35 -0400

    implemented pictures service

--- backend/routes.py
+++ backend/routes.py
@@ -35,7 +35,7 @@ def count():
 ######################################################################
 @app.route("/picture", methods=["GET"])
 def get_pictures():
-    pass
+    return jsonify(data)
 
 ######################################################################
 # GET A PICTURE
@@ -44,7 +44,11 @@ def get_pictures():
 
 @app.route("/picture/<int:id>", methods=["GET"])
 def get_picture_by_id(id):
-    pass
+    for picture in data:
+        if picture["id"] == id:
+            return picture
+    
+    return {"message": "Picture not found"}, 404
 
 
 ######################################################################
@@ -52,7 +56,27 @@ def get_picture_by_id(id):
 ######################################################################
 @app.route("/picture", methods=["POST"])
 def create_picture():
-    pass
+    new_picture = request.json
+
+    if new_picture is None:
+        return {"Message": "Invalid new picture parameter"}
+
+    try:
+        for picture in data:
+            if picture["id"] == new_picture["id"]:
+                return {"Message": f"picture with id {picture['id']} already present"}, 302
+
+        data.append(new_picture)
+        json_object = json.dumps(data, indent=4)
+
+        with open(json_url, "w") as outfile:
+            outfile.write(json_object)
+
+        return jsonify(new_picture), 201
+    except NameError:
+        return {"Message": "Invalid new picture json"}, 504
+
+    return {new_picture}, 200
 
 ######################################################################
 # UPDATE A PICTURE
@@ -61,11 +85,47 @@ def create_picture():
 
 @app.route("/picture/<int:id>", methods=["PUT"])
 def update_picture(id):
-    pass
+    new_picture = request.json
 
+    ids = [picture["id"] for picture in data]
+    if id not in ids:
+        return {"message": "picture not found"}, 404
+
+    try:
+        for idx, picture in enumerate(data):
+            if picture["id"] == id:
+
+                data[idx]["pic_url"] = new_picture["pic_url"]
+                data[idx]["event_country"] = new_picture["event_country"]
+                data[idx]["event_state"] = new_picture["event_state"]
+                data[idx]["event_city"] = new_picture["event_city"]
+                data[idx]["event_date"] = new_picture["event_date"]
+
+        json_object = json.dumps(data, indent=4)
+        with open(json_url, "w") as outfile:
+            outfile.write(json_object)
+    except NameError:
+        return {"message": "Invalid picture json"}, 504
+
+    return {"message": "success"}, 200
+    
 ######################################################################
 # DELETE A PICTURE
 ######################################################################
 @app.route("/picture/<int:id>", methods=["DELETE"])
 def delete_picture(id):
-    pass
+
+    ids = [picture["id"] for picture in data]
+    if id not in ids:
+        return {"message": "picture not found"}, 404
+
+    for picture in data:
+        if picture["id"] == id:
+            data.remove(picture)
+
+    json_object = json.dumps(data, indent=4)
+    with open(json_url, "w") as outfile:
+        outfile.write(json_object)
+
+    return {}, 204
+    
